23b801c05f36c35669b5c053cf0e6e2bb8d8ee27
KeithReshma <gopinath.chandar@gmail.com>
2021-03-22 12:51:28 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './configureStore';
+import Routes from './routes';
+// import Root from './root';
+import '../assets/sass/index.scss';
+
+const store = configureStore();
+
+ReactDOM.render(<Provider store={store} > <Routes /> </Provider>, document.getElementById('root'));
