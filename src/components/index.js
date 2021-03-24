0e5b5b52ac2e35d2d72c743241b2e92a07d226e3
praveen kumar <praveen.r@indiumsoft.com>
2021-03-25 00:03:42 +0530

    implemented sideMenuBar

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Dashboard from './Dashboard';
+import Users from './Users';
+import Companies from './Companies';
+import ManageUsers from './ManageUsers';
+import Groups from './Groups';
+import Header from './Header';
+import Footer from './Footer';
+
+export { Dashboard, Users, Companies, ManageUsers, Groups, Header, Footer };
