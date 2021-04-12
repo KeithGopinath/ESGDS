aec82cd578b85d9633a7b3a8f8dc6dfc4f1010a3
BALAJI S <I3061@ISILLT10352.indium.com>
2021-04-12 16:55:22 +0530

    conflict and header changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,18 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Avatar from 'react-avatar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faBars, faTimes, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import Dropdown from 'react-dropdown';
 
-const Header = () => {
+const Header = ({ sideBarRef }) => {
   const options = ['Super Admin', 'Employee', 'Analyst', 'Client', 'QA'];
   const defaultOption = options[0];
   const [sideBarMenuIcon, setSideBarMenuIcon] = useState(faBars);
+
+  const buttonClicklogout = () => {
+    history.push('/');
+    sessionStorage.clear();
+  };
 
   useEffect(() => {
     window.addEventListener('resize', sideMenuResponsive);
@@ -18,7 +23,7 @@ const Header = () => {
     return () => {
       window.removeEventListener('resize', sideMenuResponsive);
     };
-  },);
+  });
 
   // Function that remove class & change hamburger icon if width >= 541px
   const sideMenuResponsive = () => {
@@ -39,25 +44,31 @@ const Header = () => {
       setSideBarMenuIcon(faTimes);
     }
   };
-
   return (
     <div
       className="header-container"
     >
-      <div className="header-content-one">
+      <div className="header-content-zero content-head">
+        <button className="btn btn-light button-logout" onClick={buttonClicklogout}>
+          <FontAwesomeIcon className="signouticon" icon={faSignOutAlt} /><div className="logout-name">Logout</div>
+        </button>
+      </div>
+      <div className="header-content-one content-head">
         <Dropdown controlClassName="drop-down-element" menuClassName="drop-down-menu" options={options} value={defaultOption} placeholder="Select an option" />
       </div>
-      <div className="header-content-two">
+      <div className="header-content-two content-head">
         <Avatar name="Foo Bar" size="38" round />
       </div>
-      <div className="header-content-three">
-        <FontAwesomeIcon className="bellicon" icon={faBell} style={{ fontSize: '26px', color: '#2199c8' }} />
+      <div className="header-content-three content-head">
+        <FontAwesomeIcon className="bellicon" icon={faBell} />
       </div>
-      <div
-        className="hamburger-bars-icon"
-        onClick={sideBarMenuIconClickHandler}
-      >
-        <FontAwesomeIcon icon={sideBarMenuIcon} />
+      <div className="header-content-four content-head">
+        <div className="head-link-element">Quick Links</div>
+      </div>
+      <div className="hamburger-start">
+        <div className="hamburger-bars-icon" onClick={sideBarMenuIconClickHandler} >
+          <FontAwesomeIcon icon={sideBarMenuIcon} />
+        </div>
       </div>
     </div>
   );
