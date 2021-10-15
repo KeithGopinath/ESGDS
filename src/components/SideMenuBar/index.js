9e21f553386bd879c6eae679dbf43b2e847e083b
Praveen Kumar <praveen.r@indiumsoft.com>
2021-10-15 16:14:45 +0530

    ECS - 374/321 Added Role In REPORTS API As Params, And Enabled Reports To GroupAdmin Changes 15-10-2021

--- src/components/SideMenuBar/index.js
+++ src/components/SideMenuBar/index.js
@@ -29,7 +29,7 @@ const SideMenuBar = React.forwardRef((props, ref) => {
   // Buttonlist modified based on roles
   const modifiedButtonList = ButtonList.filter(val => (
     userRole == 'QA' || userRole == 'Analyst' ? val.id == 0 || val.id == 4 || val.id == 8 :
-      userRole == 'GroupAdmin' ? val.id == 0 || val.id == 4 || val.id == 7 || val.id == 10 :
+      userRole == 'GroupAdmin' ? val.id == 0 || val.id == 4 || val.id == 7 || val.id == 10 || val.id == 11 :
         userRole == 'Company Representative' ? val.id == 4 || val.id == 8 :
         userRole == 'Client Representative' ? val.id == 4 || val.id == 8 || val.id == 10 :
           userRole == 'SuperAdmin' || userRole == 'Admin' ? val.id !== 4 && val.id !== 8 :
--- src/sagas/Reports.js
+++ src/sagas/Reports.js
@@ -3,9 +3,9 @@ import envConfig from 'envConfig'; //eslint-disable-line
 import * as actionCreators from '../actionCreators/Reports';
 import { doGet } from '../utils/fetchWrapper';
 
-export function* getReportsRequest() {
+export function* getReportsRequest(data) {
   try {
-    const response = yield doGet(envConfig.apiEndPoints.getReports);
+    const response = yield doGet(`${envConfig.apiEndPoints.getReports}/${data.role}`);
     yield put(actionCreators.getReportsSuccess(response));
   } catch (error) {
     yield put(actionCreators.getReportsFailure(error));
