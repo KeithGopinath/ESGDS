79059bdb766ff0843c07222e988c8790f1437b74
rajeshindium <rajesh.n@indiumsoft.com>
2021-10-25 12:52:35 +0530

    custom styles changes

--- src/reducers/ForgotPassword.js
+++ src/reducers/ForgotPassword.js
@@ -12,13 +12,13 @@ export default (state = initialState, action) => {
     case 'FORGOT_PASSWORD_SUCCESS':
       return {
         ...state,
-        isLoading: true,
+        isLoading: false,
         forgotPassword: action.forgotPassword,
       };
     case 'FORGOT_PASSWORD_FAILURE':
       return {
         ...state,
-        isLoading: true,
+        isLoading: false,
         error: action.error,
       };
     default:
