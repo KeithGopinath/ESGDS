287b63f9b79d5a0efc2221ded687f7867e54b555
Jegan <gopinath.chandar@gmail.com>
2021-11-26 14:23:55 +0530

    new QA and Dev base url

--- src/config/env/baseConfig.js
+++ src/config/env/baseConfig.js
@@ -83,8 +83,10 @@ export default {
     getControversyTaskAssignment: 'taskAssignments/SuperAdmin/Controversy',
   },
   apiBaseUrl: {
-    dev: 'http://3.108.113.82/',
-    test: 'http://3.109.80.244/',
+    // dev: 'http://3.108.113.82/',
+    // test: 'http://3.109.80.244/',
+    dev: 'https://dev-backend.esgds.com/',
+    test: 'https://qa-backend.esgds.com/',
     prod: 'https://api.esgds.ai/',
   },
 };
