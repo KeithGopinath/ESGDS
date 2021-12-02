3819a0f9cc0bfc815b401da4ba5ef8e8164c3d95
Jegan <gopinath.chandar@gmail.com>
2021-12-02 20:52:16 +0530

    report pagination changes reverted

--- src/reducers/Reports.js
+++ src/reducers/Reports.js
@@ -1,20 +1,50 @@
+const initialState = {};
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'GET_REPORTS_REQUEST':
+      return {
+        ...state,
+        isLoading: true,
+        error: false,
+        reports: false,
+      };
+    case 'GET_REPORTS_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        reports: action.reports,
+      };
+    case 'GET_REPORTS_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+};
+
+// Future server pagination
 // const initialState = {};
-// export default (state = initialState, action) => {
+
+// export const pendingReportlist = (state = initialState, action) => {
 //   switch (action.type) {
-//     case 'GET_REPORTS_REQUEST':
+//     case 'GET_PENDING_REPORTLIST_REQUEST':
 //       return {
 //         ...state,
 //         isLoading: true,
 //         error: false,
-//         reports: false,
+//         // data: false,
 //       };
-//     case 'GET_REPORTS_SUCCESS':
+//     case 'GET_PENDING_REPORTLIST_SUCCESS':
 //       return {
 //         ...state,
 //         isLoading: false,
-//         reports: action.reports,
+//         error: false,
+//         data: action.pendingReportlist,
 //       };
-//     case 'GET_REPORTS_FAILURE':
+//     case 'GET_PENDING_REPORTLIST_FAILURE':
 //       return {
 //         ...state,
 //         isLoading: false,
@@ -25,86 +55,56 @@
 //   }
 // };
 
+// export const completedReportlist = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'GET_COMPLETED_REPORTLIST_REQUEST':
+//       return {
+//         ...state,
+//         isLoading: true,
+//         error: false,
+//         // data: false,
+//       };
+//     case 'GET_COMPLETED_REPORTLIST_SUCCESS':
+//       return {
+//         ...state,
+//         isLoading: false,
+//         error: false,
+//         data: action.completedReportlist,
+//       };
+//     case 'GET_COMPLETED_REPORTLIST_FAILURE':
+//       return {
+//         ...state,
+//         isLoading: false,
+//         error: action.error,
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
-const initialState = {};
-
-export const pendingReportlist = (state = initialState, action) => {
-  switch (action.type) {
-    case 'GET_PENDING_REPORTLIST_REQUEST':
-      return {
-        ...state,
-        isLoading: true,
-        error: false,
-        // data: false,
-      };
-    case 'GET_PENDING_REPORTLIST_SUCCESS':
-      return {
-        ...state,
-        isLoading: false,
-        error: false,
-        data: action.pendingReportlist,
-      };
-    case 'GET_PENDING_REPORTLIST_FAILURE':
-      return {
-        ...state,
-        isLoading: false,
-        error: action.error,
-      };
-    default:
-      return state;
-  }
-};
-
-export const completedReportlist = (state = initialState, action) => {
-  switch (action.type) {
-    case 'GET_COMPLETED_REPORTLIST_REQUEST':
-      return {
-        ...state,
-        isLoading: true,
-        error: false,
-        // data: false,
-      };
-    case 'GET_COMPLETED_REPORTLIST_SUCCESS':
-      return {
-        ...state,
-        isLoading: false,
-        error: false,
-        data: action.completedReportlist,
-      };
-    case 'GET_COMPLETED_REPORTLIST_FAILURE':
-      return {
-        ...state,
-        isLoading: false,
-        error: action.error,
-      };
-    default:
-      return state;
-  }
-};
-
-export const controversyReportlist = (state = initialState, action) => {
-  switch (action.type) {
-    case 'GET_CONTROVERSY_REPORTLIST_REQUEST':
-      return {
-        ...state,
-        isLoading: true,
-        error: false,
-        // data: false,
-      };
-    case 'GET_CONTROVERSY_REPORTLIST_SUCCESS':
-      return {
-        ...state,
-        isLoading: false,
-        error: false,
-        data: action.contorversyReportlist,
-      };
-    case 'GET_CONTROVERSY_REPORTLIST_FAILURE':
-      return {
-        ...state,
-        isLoading: false,
-        error: action.error,
-      };
-    default:
-      return state;
-  }
-};
+// export const controversyReportlist = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'GET_CONTROVERSY_REPORTLIST_REQUEST':
+//       return {
+//         ...state,
+//         isLoading: true,
+//         error: false,
+//         // data: false,
+//       };
+//     case 'GET_CONTROVERSY_REPORTLIST_SUCCESS':
+//       return {
+//         ...state,
+//         isLoading: false,
+//         error: false,
+//         data: action.contorversyReportlist,
+//       };
+//     case 'GET_CONTROVERSY_REPORTLIST_FAILURE':
+//       return {
+//         ...state,
+//         isLoading: false,
+//         error: action.error,
+//       };
+//     default:
+//       return state;
+//   }
+// };
